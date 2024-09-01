7ba2dee7a03ed8558c66d7a96d14c492009c854b
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2024-09-01 17:29:42 +0100

    added sollution parser- not working

---
+++ src/try-upgrade/SolutionParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace try_upgrade
+{
+    public class SolutionParser
+    {
+        public static List<string> ExtractCsprojFiles(string solutionFilePath)
+        {
+            var csprojFilePaths = new List<string>();
+
+            try
+            {
+                XDocument xdoc = XDocument.Load(solutionFilePath);
+                var projectNodes = xdoc.Descendants("Project");
+
+                foreach (var projectNode in projectNodes)
+                {
+                    var projectFilePathAttribute = projectNode.Attribute("FilePath");
+                    if (projectFilePathAttribute != null)
+                    {
+                        var projectFilePath = projectFilePathAttribute.Value;
+                        if (projectFilePath.EndsWith(".csproj"))
+                        {
+                            csprojFilePaths.Add(projectFilePath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error parsing solution file: {ex.Message}");
+            }
+
+            return csprojFilePaths;
+        }
+    }
+}
---
+++ tests/try-upgrade.Tests/SolutionParserTests.cs
@@ -0,0 +1,86 @@
+﻿namespace try_upgrade.Tests
+{
+    public class SolutionParserTests
+    {
+        [Fact]
+        public void ExtractCsprojFiles_ShouldExtractCorrectCsprojPaths()
+        {
+            // Arrange
+            string solutionContent = @"
+                Microsoft Visual Studio Solution File, Format Version 12.00
+                # Visual Studio 15
+                Project(""E3E38632-312D-454C-912E-DB664B2AE92F"") = ""Project1"", ""Project1.csproj"", ""{FAE04EC0-301B-11D3-BF4B-00C04F79EFBC}""
+                EndProject
+                Global
+                GlobalSection(SolutionConfigurationPlatforms) = preSolution
+                EndGlobalSection
+                GlobalSection(ProjectConfigurationPlatforms) = postSolution
+                EndGlobalSection   
+
+                EndGlobal
+            ";
+
+            // Act
+            List<string> csprojFilePaths = SolutionParser.ExtractCsprojFiles(solutionContent);
+
+            // Assert
+            Assert.Single(csprojFilePaths);
+            Assert.Equal("Project1.csproj", csprojFilePaths[0]);
+        }
+
+        [Fact]
+        public void ExtractCsprojFiles_ShouldHandleInvalidSolutionContent()
+        {
+            // Arrange
+            string invalidSolutionContent = "Invalid solution content";
+
+            // Act
+            List<string> csprojFilePaths = SolutionParser.ExtractCsprojFiles(invalidSolutionContent);
+
+            // Assert
+            Assert.Empty(csprojFilePaths);
+        }
+
+        [Fact]
+        public void ExtractCsprojFiles_ShouldHandleEmptySolutionContent()
+        {
+            // Arrange
+            string emptySolutionContent = "";
+
+            // Act
+            List<string> csprojFilePaths = SolutionParser.ExtractCsprojFiles(emptySolutionContent);
+
+            // Assert
+            Assert.Empty(csprojFilePaths);
+        }
+
+        [Fact]
+        public void ExtractCsprojFiles_ShouldHandleMultipleCsprojFiles()
+        {
+            // Arrange
+            string solutionContent = @"
+                Microsoft Visual Studio Solution File, Format Version 12.00
+                # Visual Studio 15
+                Project(""E3E38632-312D-454C-912E-DB664B2AE92F"") = ""Project1"", ""Project1.csproj"", ""{FAE04EC0-301B-11D3-BF4B-00C04F79EFBC}""
+                EndProject
+                Project(""E3E38632-312D-454C-912E-DB664B2AE92F"") = ""Project2"", ""Project2.csproj"", ""{FAE04EC0-301B-11D3-BF4B-00C04F79EFBC}""
+                EndProject
+                Global
+                GlobalSection(SolutionConfigurationPlatforms) = preSolution
+                EndGlobalSection
+                GlobalSection(ProjectConfigurationPlatforms) = postSolution
+                EndGlobalSection   
+
+                EndGlobal
+            ";
+
+            // Act
+            List<string> csprojFilePaths = SolutionParser.ExtractCsprojFiles(solutionContent);
+
+            // Assert
+            Assert.Equal(2, csprojFilePaths.Count);
+            Assert.Contains("Project1.csproj", csprojFilePaths);
+            Assert.Contains("Project2.csproj", csprojFilePaths);
+        }
+    }
+}
